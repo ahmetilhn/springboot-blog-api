8ac17a8097fd185f356da421e958354ca3e740e5
Ahmet ilhan <ailhan@hepsiemlak.com>
2023-08-12 20:34:53 +0300

    status codes added

--- src/main/java/com/ahmetilhan/blogapi/controller/PostController.java
+++ src/main/java/com/ahmetilhan/blogapi/controller/PostController.java
@@ -3,6 +3,8 @@ package com.ahmetilhan.blogapi.controller;
 import com.ahmetilhan.blogapi.model.PostModel;
 import com.ahmetilhan.blogapi.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Array;
@@ -15,10 +17,12 @@ public class PostController {
     @Autowired
     private PostService service;
     @GetMapping
-    public List<PostModel> getAllPosts(){
-        return service.getAllPosts();
+    public ResponseEntity<List<PostModel>> getAllPosts(){
+        List<PostModel> posts = service.getAllPosts();
+        return new ResponseEntity<List<PostModel>>(posts, HttpStatus.OK);
     }
     @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
     public void create(@RequestBody PostModel payload){
         service.create(payload);
     }
