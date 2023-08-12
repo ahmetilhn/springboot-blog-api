ff50fdcfc450973800a54e5022cd68bb30330f94
Ahmet ilhan <ailhan@hepsiemlak.com>
2023-08-13 00:34:48 +0300

    mongoDb collection name added

--- src/main/java/com/ahmetilhan/blogapi/model/PostModel.java
+++ src/main/java/com/ahmetilhan/blogapi/model/PostModel.java
@@ -2,7 +2,7 @@ package com.ahmetilhan.blogapi.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(collection = "posts")
 public class PostModel{
     @Id
     private String _id;
